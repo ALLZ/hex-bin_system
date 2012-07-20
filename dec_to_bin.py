2231f904684a17096f37fb6d07ccbed2bd2b5661
Alexandr <sazalexnev@gmail.com>
2012-07-20 16:43:15 +0400

    add plugin

---
+++ dec_to_bin.py
@@ -0,0 +1,18 @@
+import sublime, sublime_plugin
+
+class DecToBinCommand(sublime_plugin.TextCommand):
+    MAX_STR_LEN = 30
+    def run(self, edit):
+        v = self.view
+
+        dec = v.substr(v.sel()[0])
+
+        if dec.isdigit():
+            v.replace(edit, v.sel()[0], bin(int(dec))[2:].upper())
+        else:
+            if len(dec) > self.MAX_STR_LEN:
+                logMsg = dec[0:self.MAX_STR_LEN]+ "..."
+            else:
+                logMsg = dec
+            sublime.status_message("\"" + logMsg + "\" isn't a decimal number!")
+            sublime.error_message("\"" + logMsg + "\" isn't a decimal number!")

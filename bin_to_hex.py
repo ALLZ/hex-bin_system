ff35e4ca64a4dc5a2dab4ee2c355e61f080a2139
Alex Brem <alex@fluktuation.net>
2013-12-19 17:45:11 +0100

    Add BinToHexCommand and HexToBinCommand.

---
+++ bin_to_hex.py
@@ -0,0 +1,28 @@
+import sublime
+import sublime_plugin
+
+
+class BinToHexCommand(sublime_plugin.TextCommand):
+    MAX_STR_LEN = 20
+
+    def run(self, edit):
+        v = self.view
+        reglist = list(v.sel())
+        for item in reglist:
+            bin = v.substr(v.sel()[reglist.index(item)])
+            bin = bin.strip()
+            l = True
+            if bin == '':
+                l = False
+            for i in bin:
+                if not((i == '1') or (i == '0')):
+                    l = False
+            if l:
+                v.replace(edit, v.sel()[reglist.index(item)], str(hex(int(bin, 2))[2:].upper()))
+            else:
+                if len(bin) > self.MAX_STR_LEN:
+                    logMsg = bin[0:self.MAX_STR_LEN] + "..."
+                else:
+                    logMsg = bin
+                sublime.status_message("\"%s\" isn't a binary number!" % logMsg)
+                sublime.error_message("\"%s\" isn't a binary number!" % logMsg)

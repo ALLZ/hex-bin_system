6a7b0e203b595fc11d71341eaf28e439bb67d99e
Alexandr <sazalexnev@gmail.com>
2014-02-22 17:23:47 +0400

    Upd: fix
    Fixed 1 bug and some tabs

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -8,11 +8,19 @@ class DecToBinCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         v = self.view
         reglist = list(v.sel())
+<<<<<<< HEAD
+        for i in range(0, len(reglist)):
+            dec = v.substr(v.sel()[i])
+            dec = dec.strip()
+            if dec.isdigit():
+                v.replace(edit, v.sel()[i], '{0:b}'.format(int(dec)))
+=======
         for item in reglist:
             dec = v.substr(v.sel()[reglist.index(item)])
             dec = dec.strip()
             if dec.isdigit():
                 v.replace(edit, v.sel()[reglist.index(item)], bin(int(dec))[2:].upper())
+>>>>>>> 736565c4382dadd2d10eeb9626dae94fe68a70a3
             else:
                 if len(dec) > self.MAX_STR_LEN:
                     logMsg = dec[0:self.MAX_STR_LEN] + "..."
--- hex_to_dec.py
+++ hex_to_dec.py
@@ -8,8 +8,13 @@ class HexToDecCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         v = self.view
         reglist = list(v.sel())
+<<<<<<< HEAD
+        for j in range(0, len(reglist)):
+            hx = v.substr(v.sel()[j])
+=======
         for item in reglist:
             hx = v.substr(v.sel()[reglist.index(item)])
+>>>>>>> 736565c4382dadd2d10eeb9626dae94fe68a70a3
             hx = hx.strip()
             hexdig = '0123456789abcdefABCDEF'
             l = True
@@ -20,7 +25,11 @@ class HexToDecCommand(sublime_plugin.TextCommand):
                     l = False
 
             if l:
+<<<<<<< HEAD
+                v.replace(edit, v.sel()[j], str(int(hx, 16)))
+=======
                 v.replace(edit, v.sel()[reglist.index(item)], str(int(hx, 16)))
+>>>>>>> 736565c4382dadd2d10eeb9626dae94fe68a70a3
             else:
                 if len(hx) > self.MAX_STR_LEN:
                     logMsg = hx[0:self.MAX_STR_LEN] + "..."
